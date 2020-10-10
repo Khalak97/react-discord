5f5233e2c9d5c25c4c47ea07dd0730b2b9e09799
Khalak97 <valodyakh@gmail.com>
2020-10-10 09:13:13 +0300

    init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA1qQg2EyEsGFckMyxKDZESZ5gOnsKopAs",
+  authDomain: "react-discord.firebaseapp.com",
+  databaseURL: "https://react-discord.firebaseio.com",
+  projectId: "react-discord",
+  storageBucket: "react-discord.appspot.com",
+  messagingSenderId: "1074536859914",
+  appId: "1:1074536859914:web:f21f8159dbe700365992f3",
+  measurementId: "G-6DRB429Y0F",
+};
